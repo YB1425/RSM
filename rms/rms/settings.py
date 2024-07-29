482c6a1449be3f64689cfca7147ffaefd41736ae
YB1425 <bashayasser13@gmail.com>
2024-07-29 10:21:39 +0300

    esraa:added new admin Interface and demo video

--- rms/rms/settings.py
+++ rms/rms/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -28,29 +28,27 @@ ALLOWED_HOSTS = []
 
 # Application definition
 INSTALLED_APPS = [
-   'django.contrib.admin',
-   'django.contrib.auth',
-   'django.contrib.contenttypes',
-   'django.contrib.sessions',
-   'django.contrib.messages',
-   'django.contrib.staticfiles',
-   'rest_framework',
-   'rest_framework.authtoken',
-   'system',
+    'admin_interface',
+    'colorfield',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'system',
 ]
-
 
 AUTH_USER_MODEL = 'system.CustomUser'
 
-
 REST_FRAMEWORK = {
-   'DEFAULT_AUTHENTICATION_CLASSES': [
-       'rest_framework.authentication.TokenAuthentication',
-   ],
-
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 
 }
-
 
 DATABASES = {
     'default': {
@@ -125,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- rms/system/views.py
+++ rms/system/views.py
@@ -6,10 +6,11 @@ from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework import status , viewsets
-from django.shortcuts import render , redirect
+from rest_framework import status, viewsets
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
+
 
 def reset_password(request):
     if request.method == 'POST':
@@ -35,83 +36,82 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
 
+
 class RestaurantViewSet(viewsets.ModelViewSet):
     queryset = Diner.objects.all()
     serializer_class = DinerSerializer
+
 
 class MenuViewSet(viewsets.ModelViewSet):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
 
+
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = CustomerOrder.objects.all()
     serializer_class = CustomerOrderSerializer
+
 
 class ReservationViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
 
+
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Feedback.objects.all()
     serializer_class = FeedbackSerializer
+
 
 class IngredientViewSet(viewsets.ModelViewSet):
     queryset = Supply.objects.all()
     serializer_class = SupplySerializer
 
+
 class StaffViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
 
+
 @api_view(['POST'])
 def register_user(request):
-   if request.method == 'POST':
-       serializer = UserSerializer(data=request.data)
-       if serializer.is_valid():
-           serializer.save()
-           return Response(serializer.data, status=status.HTTP_201_CREATED)
-       return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
+    if request.method == 'POST':
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['POST'])
 def user_login(request):
-   if request.method == 'POST':
-       username = request.data.get('username')
-       password = request.data.get('password')
+    if request.method == 'POST':
+        username = request.data.get('username')
+        password = request.data.get('password')
 
+        user = None
+        if '@' in username:
+            try:
+                user = CustomUser.objects.get(email=username)
+            except ObjectDoesNotExist:
+                pass
 
-       user = None
-       if '@' in username:
-           try:
-               user = CustomUser.objects.get(email=username)
-           except ObjectDoesNotExist:
-               pass
+        if not user:
+            user = authenticate(username=username, password=password)
 
+        if user:
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key}, status=status.HTTP_200_OK)
 
-       if not user:
-           user = authenticate(username=username, password=password)
-
-
-       if user:
-           token, _ = Token.objects.get_or_create(user=user)
-           return Response({'token': token.key}, status=status.HTTP_200_OK)
-
-
-       return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
-
-
+        return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
 
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def user_logout(request):
-   if request.method == 'POST':
-       try:
-           # Delete the user's token to logout
-           request.user.auth_token.delete()
-           return Response({'message': 'Successfully logged out.'}, status=status.HTTP_200_OK)
-       except Exception as e:
-           return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
+    if request.method == 'POST':
+        try:
+            # Delete the user's token to logout
+            request.user.auth_token.delete()
+            return Response({'message': 'Successfully logged out.'}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
